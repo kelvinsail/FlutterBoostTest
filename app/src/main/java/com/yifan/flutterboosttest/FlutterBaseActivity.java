498825566618381f45ec539dd70dd8eab09dab1d
yifan <kelvinsail@163.com>
2023-05-05 23:48:17 +0800

    flutterBoost页面容器实现

---
+++ app/src/main/java/com/yifan/flutterboosttest/FlutterBaseActivity.java
@@ -0,0 +1,13 @@
+package com.yifan.flutterboosttest;
+
+import com.idlefish.flutterboost.containers.FlutterBoostActivity;
+
+/**
+ * @author yifan Wu
+ *
+ * created in 2023/5/5.
+ *
+ * used for
+ */
+public class FlutterBaseActivity extends FlutterBoostActivity {
+}
